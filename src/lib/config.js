e77d6484bbc4409ac78ec93f8c3eebae544be0e0
LuisSevillano <luissevillanopires@gmail.com>
2025-01-22 18:27:29 +0100

    Update config

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@ export const siteTitle = 'Nombres de Cooperativas en Castilla y León 📋';
 export const siteDescription =
 	'Explora nombres inspiradores de cooperativas en Castilla y León, ordena, busca y filtra según tus intereses.';
 export const siteLongDesc =
-	'Esta página ofrece un listado interactivo de cooperativas registradas en Castilla y León. Podrás buscar, ordenar y filtrar por diversos criterios para inspirarte en la propuesta de tu propio nombre.';
+	'Esta página ofrece un listado interactivo de cooperativas registradas en Castilla y León. Podrás buscar, ordenar y filtrar por diversos criterios para inspirarte o comprobar si tu idea ya existe.';
 export const siteURL = 'https://luissevillano.net/registro-cooperativas-cyl/index.html';
 export const home = 'https://luissevillano.net';
 export const siteImage = 'https://luissevillano.net/registro-cooperativas-cyl/cover.jpg';
